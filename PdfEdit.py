88afcaa2760ee323fc2fe3503820ecda113e2c90
tzous <tzous@126.com>
2019-01-29 22:51:27 +0800

    listArea

--- PdfEdit.py
+++ PdfEdit.py
@@ -9,6 +9,8 @@ import os
 import os.path
 import shutil
 
+import fitz
+
 import six
 
 
@@ -20,9 +22,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-        self.bOpened = False    #文档是否打开
+        if hasattr(sys, '_MEIPASS'):
+            # PyInstaller会创建临时文件夹temp，并把路径存储在_MEIPASS中
+            self.appPath = os.path.dirname(os.path.realpath(sys.executable))
+        else:
+            self.appPath, filename = os.path.split(os.path.abspath(__file__))
+        self.bOpened = True     # 文档是否打开
+        self.nPages = 0      # 文档总页数
+        self.nCurr = 0       # 当前文档页码
+        self.docDoc = None      # 当前pymupdf文档对象
+        self.nMaxPages = 32   # 最大显示页数
+        self.IMAGE_SIZE = QSize(180,250)
 
-        self.set_menu_status()  #初始化菜单项状态
+        self.set_menu_status()  # 初始化菜单项状态
+        self.initEventPlot()  # 初始化事件
 
     # 设置菜单项状态
     def set_menu_status(self):
@@ -37,8 +50,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.actionMove.setEnabled(True)
             self.actionDelete.setEnabled(True)
             self.actionRotate.setEnabled(True)
-            self.indexArea.setVisible(True)
-            self.displayArea.setVisible(True)
+            self.listArea.setVisible(True)
+            self.showArea.setVisible(True)
         else:
             self.actionSave.setEnabled(False)
             self.actionSaveAs.setEnabled(False)
@@ -50,9 +63,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.actionMove.setEnabled(False)
             self.actionDelete.setEnabled(False)
             self.actionRotate.setEnabled(False)
-            self.indexArea.setVisible(False)
-            self.displayArea.setVisible(False)
+            self.listArea.setVisible(False)
+            self.showArea.setVisible(False)
 
+    # 初始化事件
+    def initEventPlot(self):
+        # 打开文件
+        self.actionOpen.triggered.connect(self.onClickedactionOpen)
+
+    # 打开文件
+    def onClickedactionOpen(self):
+        pdfName, pdfType = QFileDialog.getOpenFileName(self, "打开pdf文件", "", "*.pdf")
+        if pdfName:
+            self.docDoc = fitz.open(pdfName)
+            widget = QWidget(self)
+            vboxLayout = QVBoxLayout()
+            self.nPages = self.docDoc.pageCount
+            for i in range(0, self.nPages):
+                label = QLabel(self)
+                page = self.docDoc[i]
+                zoom = int(100)
+                rotate = int(0)
+                trans = fitz.Matrix(zoom / 100.0, zoom / 100.0).preRotate(rotate)
+                pix = page.getPixmap(matrix=trans, alpha=False)
+                fmt = QImage.Format_RGBA8888 if pix.alpha else QImage.Format_RGB888
+                qtimg = QImage(pix.samples, pix.width, pix.height, pix.stride, fmt)
+                label.setPixmap(QPixmap.fromImage(qtimg).scaled(self.IMAGE_SIZE))
+                vboxLayout.addWidget(label)
+            widget.setLayout(vboxLayout)
+            self.listArea.setWidget(widget)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
